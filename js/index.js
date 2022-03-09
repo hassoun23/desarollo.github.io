a196b7b05a6c0428743f27b4facb988296218ad1
Hassan Hamze <Hassan2393@gmail.com>
2022-03-09 14:33:01 -0300

    pre entrega 1

--- js/index.js
+++ js/index.js
@@ -1,45 +1,41 @@
 const productos = [
-    { producto: "dove aerosol", tipo: "aerosol", precio: 200 },
-    { producto: "dove barra", tipo: "barra", precio: 250 },
-    { producto: "aktiol", tipo: "alcohol", precio: 120 },
-    { producto: "duracell", tipo: "d", precio: 185 },
-    { producto: "gillette match 3", tipo: "match 3", precio: 235 },
-    { producto: "gilette venus", tipo: "venus", precio: 100},
+  { producto: "dove aerosol", tipo: "aerosol", precio: 200 },
+  { producto: "dove barra", tipo: "barra", precio: 250 },
+  { producto: "aktiol", tipo: "alcohol", precio: 120 },
+  { producto: "duracell", tipo: "d", precio: 185 },
+  { producto: "gillette match 3", tipo: "match 3", precio: 235 },
+  { producto: "gilette venus", tipo: "venus", precio: 100},
+];
 
-  ];
+const carrito = [];
 
-  const carrito = [];
-  
-  
-  const total = carrito.reduce((acc,el) => acc + el.precio,0);
-  
+
 let miPedido;
+let finalizacion;
 
-while (miPedido !=""){
-  
-  miPedido = prompt("ingrese un producto o de lo contrario haga click en Ok");
-  
+do{
+
+  miPedido = prompt("ingrese un pedido o ingrese finalizar pedido");
+
   const seleccionProducto = productos.find(productos => productos.producto === miPedido);
-  
+
   carrito.push(seleccionProducto);
-  
-  console.log(seleccionProducto);
-  
-  console.log(total);
-  
-  
-  
-  if(miPedido === ""){
-    
-    
-    alert(`su pedido finalizo su cuenta es  ${total}`);
-    
-    
-  }
-  
-  
+
+  finalizacion = prompt('1. Termina pedido \n2.Sigue haciendo pedidos')
+
+} while ( finalizacion !== "1"){
+
+  console.log(carrito);
 }
 
-  
 
+const total = (array) => {
+  return array.reduce((acc, el) => acc + el.precio,0)
+};
 
+const lista = (array) => {
+    return array.map((data) => `${data.producto} : $${data.precio}.\n`)
+        
+}
+
+alert (`su cuenta es un total de $${total(carrito)} \n\n Carrito:\n\n${lista(carrito)}`);
